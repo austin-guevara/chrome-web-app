e14cadc13d87ec74fb62b5d1bf284890435c8a5f
Austin Guevara <a.guevara@salesforce.com>
2018-05-21 11:49:15 -0400

    Add web page source files

---
+++ cloud-apps/js/index.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+
+	var a = document.getElementById("actionButton");
+	
+	function navigatePage() {
+		var url = document.getElementById("inputURL").value;
+		window.open(url,"_self");
+	}
+
+	a.onclick = function() {
+		navigatePage();
+	};
+	
+	document.addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+			navigatePage();
+    }
+});
+};
